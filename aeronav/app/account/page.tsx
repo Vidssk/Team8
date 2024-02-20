708f07ee5c76b8842f3f583b39d70e458d0778b7
Vidssk <santiagohernandez120199@gmail.com>
2024-02-20 10:13:05 -0600

    account

---
+++ aeronav/app/account/page.tsx
@@ -0,0 +1,12 @@
+import '@/styles/account.css'
+
+const page = () => {
+  return (
+    <div className='account-page-wrapper'>
+        <h1>Account Page</h1>
+  </div>
+
+  )
+}
+
+export default page
