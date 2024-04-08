811b1bd49fd60ad4b1f5b14d20ceb66d15df3c7e
Vidssk <santiagohernandez120199@gmail.com>
2024-04-08 11:54:31 -0500

    Started on Navigation Menu

--- AeroNavMobile2/app/src/main/java/com/example/aeronavmobile/ItineraryActivity.java
+++ AeroNavMobile2/app/src/main/java/com/example/aeronavmobile/ItineraryActivity.java
@@ -1,7 +1,6 @@
 package com.example.aeronavmobile;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ItineraryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_itinerary);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
