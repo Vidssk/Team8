144b9ca23780762add136d9ea32fe2b9d3b32d8a
Vidssk <santiagohernandez120199@gmail.com>
2024-04-08 23:46:40 -0500

    Started on Navigation Menu

--- aeronav/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import SurveyPage from "@/components/SurveyPage"
-import '@/styles/globals.css'
-
-export default function Home() {
-  return (
-    <SurveyPage />
-  );
-}
